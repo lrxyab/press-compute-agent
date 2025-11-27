89d0bd60d6f1bd1f7509b642ea67d41b1ae2ffab
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2025-11-28 04:22:30 +0530

    feat: compute settings for agents and orchestrators both

---
+++ agent/agent/doctype/compute_settings/compute_settings.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2025, ayush@frappe.io and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ComputeSettings(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		is_orchestrator: DF.Check
+	# end: auto-generated types
+
+	pass
