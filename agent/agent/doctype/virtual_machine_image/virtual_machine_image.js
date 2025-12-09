091ee5206f626477554f75a5b6b62220ad299ce0
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2025-12-09 12:11:06 +0530

    feat: VM images

---
+++ agent/agent/doctype/virtual_machine_image/virtual_machine_image.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, ayush@frappe.io and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Virtual Machine Image", {
+// 	refresh(frm) {
+
+// 	},
+// });
