934cac110a132940b0c3a8743493fc5aef71029e
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-01-14 16:17:15 +0530

    feat: Virtual Machine Images

--- agent/agent/doctype/virtual_machine_image/virtual_machine_image.js
+++ agent/agent/doctype/virtual_machine_image/virtual_machine_image.js
@@ -1,8 +1,42 @@
 // Copyright (c) 2025, ayush@frappe.io and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Virtual Machine Image", {
-// 	refresh(frm) {
+// Copied from Press
 
-// 	},
-// });
+frappe.ui.form.on('Virtual Machine Image', {
+	refresh: function (frm) {
+		[
+			[__('Take Image'), 'take_image', false, frm.doc.is_from_vm && frm.doc.status !== "Completed"],
+		].forEach(([label, method, confirm, condition]) => {
+			if (typeof condition === 'undefined' || condition) {
+				frm.add_custom_button(
+					label,
+					() => {
+						if (confirm) {
+							frappe.confirm(
+								`Are you sure you want to ${label.toLowerCase()}?`,
+								() =>
+									frm.call(method).then((r) => {
+										if (r.message) {
+											frappe.msgprint(r.message);
+										} else {
+											frm.refresh();
+										}
+									}),
+							);
+						} else {
+							frm.call(method).then((r) => {
+								if (r.message) {
+									frappe.msgprint(r.message);
+								} else {
+									frm.refresh();
+								}
+							});
+						}
+					},
+					__('Actions'),
+				);
+			}
+		});
+	},
+});
--- agent/agent/doctype/virtual_machine_image/virtual_machine_image.py
+++ agent/agent/doctype/virtual_machine_image/virtual_machine_image.py
@@ -1,10 +1,13 @@
 # Copyright (c) 2025, ayush@frappe.io and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from pathlib import Path
+from uuid import uuid4
 from agent.utils import is_orchestrator
 from frappe.model.document import Document
-
+from agent.agent.backup_lib.backup import VMBackup
+from agent.configuration.paths import CONFIG_PATH
 
 class VirtualMachineImage(Document):
 	# begin: auto-generated types
@@ -15,8 +18,10 @@ class VirtualMachineImage(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		amended_from: DF.Link | None
-		file_path: DF.Data
+		file_path: DF.Data | None
+		is_from_vm: DF.Check
+		status: DF.Literal["Draft", "Pending", "Completed"]
+		virtual_machine: DF.Link | None
 	# end: auto-generated types
 
 	pass
@@ -33,3 +38,17 @@ class VirtualMachineImage(Document):
 			call_to_agent = ComputeCall(self.agent)
 			doc_dict = call_to_agent.update_doc(self.doctype, self.name, self.as_dict())
 			self.update(doc_dict)
+
+	@frappe.whitelist()
+	def take_image(self):
+		frappe.enqueue_doc("Virtual Machine Image", self.name, "_take_image")
+
+	def _take_image(self):
+		image_path = Path(CONFIG_PATH, "images", f"{uuid4()}.qcow2")
+		virtual_machine = frappe.get_doc("Virtual Machine", self.virtual_machine)
+		backup = VMBackup(virtual_machine.domain)
+		backup.backup_disk("vda", str(image_path.absolute()))
+		backup.begin()
+		self.file_path = str(image_path.absolute())
+		self.status = "Completed"
+		self.save()
