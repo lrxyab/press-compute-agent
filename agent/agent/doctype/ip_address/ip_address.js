ae7617b2eb3a5019e37d0f46450e6b57495b2441
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-02-27 12:10:45 +0530

    feat: move ip addresses to orchestrator

--- agent/agent/doctype/ip_address/ip_address.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, ayush@frappe.io and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("IP Address", {
-// 	refresh(frm) {
-
-// 	},
-// });
