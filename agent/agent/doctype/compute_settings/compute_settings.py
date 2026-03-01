76b4fa3411d5f63543c5f878f0dc8e634899c169
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-03-02 02:31:50 +0530

    fix: Supply the base url of the orchestrator in Compute Settings

--- agent/agent/doctype/compute_settings/compute_settings.py
+++ agent/agent/doctype/compute_settings/compute_settings.py
@@ -17,6 +17,7 @@ class ComputeSettings(Document):
 		default_network_interface: DF.Data | None
 		orchestrator_api_key: DF.Data | None
 		orchestrator_api_secret: DF.Password | None
+		orchestrator_base_url: DF.Data | None
 		private_ip: DF.Data | None
 		private_network_interface: DF.Data | None
 	# end: auto-generated types
