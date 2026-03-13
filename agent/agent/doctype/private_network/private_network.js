7e427ffe6730f8a5019fbed3521ec5df58555456
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-03-14 03:07:02 +0530

    fix: Move private networks to orchestrator

--- agent/agent/doctype/private_network/private_network.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, ayush@frappe.io and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Private Network", {
-// 	refresh(frm) {
-
-// 	},
-// });
