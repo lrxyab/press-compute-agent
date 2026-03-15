7ab4d784a42e3263591e223a2c11e42f907fe7e6
Ayush Chaudhari <ayushschaudhari1904@gmail.com>
2026-03-15 15:40:50 +0000

    fix: Move virtual_machine.py to API directory

--- agent/api/virtual_machine.py
+++ agent/api/virtual_machine.py
@@ -3,5 +3,5 @@ import frappe
 
 @frappe.whitelist(methods=["POST"])
 def stop(instance_id: str):
-	virtual_machine_doc = frappe.get_doc("Virtual Machine", {"uuid": instance_idt})
+	virtual_machine_doc = frappe.get_doc("Virtual Machine", {"uuid": instance_id})
 	virtual_machine_doc.stop()
