2dce8c92950895e692541258696d3cc228f2b349
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-03-15 21:22:56 +0530

    feat: Start VM API

--- agent/api/virtual_machine.py
+++ agent/api/virtual_machine.py
@@ -5,3 +5,9 @@ import frappe
 def stop(instance_id: str, force=False):
 	virtual_machine_doc = frappe.get_doc("Virtual Machine", {"uuid": instance_id})
 	virtual_machine_doc.stop(force)
+
+
+@frappe.whitelist(methods=["POST"])
+def start(instance_id: str):
+	virtual_machine_doc = frappe.get_doc("Virtual Machine", {"uuid": instance_id})
+	virtual_machine_doc.start()
