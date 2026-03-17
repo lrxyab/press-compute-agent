a5ca333c2df45898a6a5a02ebb7559e327dab2df
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-03-17 14:46:39 +0530

    feat: Reboot API

--- agent/api/virtual_machine.py
+++ agent/api/virtual_machine.py
@@ -13,7 +13,7 @@ def start(instance_id: str):
 	virtual_machine_doc.start()
 
 
-@frappe.whitelist()
+@frappe.whitelist(methods=["POST"])
 def terminate(instance_id: str):
 	name = frappe.get_value("Virtual Machine", {"uuid": instance_id}, "name")
 
@@ -24,3 +24,9 @@ def terminate(instance_id: str):
 	vm_doc = frappe.get_doc("Virtual Machine", name)
 	vm_doc.delete()
 	return "SUCCESS"
+
+
+@frappe.whitelist(methods=["POST"])
+def reboot(instance_id: str):
+	virtual_machine_doc = frappe.get_doc("Virtual Machine", {"uuid": instance_id})
+	virtual_machine_doc.reboot()
