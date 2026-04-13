4ee650ec7df9083ba826993d842db9ec5e5c8b66
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-04-13 12:15:30 +0530

    feat: Resize the disk of a running VM

--- agent/agent/doctype/disk/disk.py
+++ agent/agent/doctype/disk/disk.py
@@ -4,6 +4,7 @@
 import os
 import shutil
 import subprocess
+from typing import TYPE_CHECKING
 from uuid import uuid4
 
 import frappe
@@ -12,6 +13,10 @@ from frappe.model.document import Document
 from frappe.utils.password import get_decrypted_password
 
 from agent.agent.ceph_lib.ceph import Ceph
+
+if TYPE_CHECKING:
+	from agent.agent.doctype.virtual_machine.virtual_machine import VirtualMachine
+
 from agent.configuration.paths import CONFIG_PATH
 
 DISKS_ROOT = os.path.join(CONFIG_PATH, "disks")
@@ -115,8 +120,26 @@ class Disk(Document):
 		match self.storage_medium:
 			case "File":
 				size = int(self.size)
-				subprocess.call(
-					["qemu-img", "resize", "-f", "qcow2", self.get_path(), f"{size}G"], shell=False
-				)
+				if self.use_qemu_resize():
+					subprocess.check_output(
+						["qemu-img", "resize", "-f", "qcow2", self.get_path(), f"{size}G"], shell=False
+					)
+				else:
+					self.increase_vm_disk_size()
+
 			case "Ceph":
 				self.ceph.resize(self.size)
+
+	def use_qemu_resize(self):
+		virtual_machine = frappe.db.get_value("VM Disk", {"disk": self.name}, "parent")
+		if not virtual_machine:
+			return True
+		virtual_machine_doc = frappe.get_doc("Virtual Machine", virtual_machine)
+		if virtual_machine_doc.state != "Running":
+			return True
+		return False
+
+	def increase_vm_disk_size(self):
+		disk_entry = frappe.db.get_value("VM Disk", {"disk": self.name}, ["parent", "device"], as_dict=True)
+		virtual_machine_doc: VirtualMachine = frappe.get_doc("Virtual Machine", disk_entry.parent)
+		virtual_machine_doc.domain.blockResize(disk_entry.device, self.size * (1024**2))
