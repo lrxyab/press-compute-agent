db2aa050c769d902d7fe79030c613052ff3452ea
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-04-16 15:41:38 +0530

    chore: Type hints for VMI API

--- agent/api/virtual_machine_image.py
+++ agent/api/virtual_machine_image.py
@@ -1,10 +1,15 @@
+from typing import TYPE_CHECKING
+
 import frappe
+
+if TYPE_CHECKING:
+	from agent.agent.doctype.virtual_machine_image.virtual_machine_image import VirtualMachineImage
 
 
 @frappe.whitelist()
 def new(instance_id: str):
 	virtual_machine = frappe.db.get_value("Virtual Machine", {"uuid": instance_id})
-	virtual_machine_image_doc = frappe.new_doc("Virtual Machine Image")
+	virtual_machine_image_doc: VirtualMachineImage = frappe.new_doc("Virtual Machine Image")
 	virtual_machine_image_doc.name = f"{virtual_machine}-image-{frappe.utils.random_string(5)}"
 	virtual_machine_image_doc.virtual_machine = virtual_machine
 	virtual_machine_image_doc.is_from_vm = True
