99015c9acda123407cdc4249072435d9cd50bc2c
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-05-11 13:46:03 +0530

    fix: Load from DB snapshots

--- agent/agent/doctype/snapshot/snapshot.py
+++ agent/agent/doctype/snapshot/snapshot.py
@@ -99,6 +99,7 @@ class BaseSnapshot(Document):
 		self.sha256sum = get_sha256sum_of_file(self.file_path)
 		self.progress = 100
 		self.save()
+		self.load_from_db()
 
 		"""
 			If it's a snapshot, we need to upload it to S3 after taking the image, but for a virtual machine image, we don't want to do that at all because it's only used for creating disks and attaching to VMs, not for backup purposes and because we want to keep the image locally for faster disk creation.
@@ -118,7 +119,7 @@ class BaseSnapshot(Document):
 			"_take_image_file",
 			enqueue_after_commit=True,
 			queue="long",
-			timeout=7200,
+			timeout=18000,
 		)
 
 	def create_disk_from_image(self, size: int):
@@ -170,6 +171,7 @@ class BaseSnapshot(Document):
 		self.status = "Pending"
 		s3_client.upload_file(self.file_path, creds.bucket_name, Key=self.name)
 		self.save()
+		self.load_from_db()
 
 	def _upload_file_to_s3_and_delete_local(self):
 		self._upload_file_to_s3()
@@ -179,6 +181,7 @@ class BaseSnapshot(Document):
 
 		self.status = "Available"
 		self.save()
+		self.load_from_db()
 
 	@frappe.whitelist(methods=["POST"])
 	def sync(self):
