b3a89880dd7e48ee30dd0b676dd9700b74bd74bd
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-05-11 11:27:20 +0530

    feat: Snapshot filepath early commit

--- agent/agent/doctype/snapshot/snapshot.py
+++ agent/agent/doctype/snapshot/snapshot.py
@@ -56,6 +56,7 @@ class BaseSnapshot(Document):
 	def _take_image_file(self):  # noqa: C901
 		image_path = Path(CONFIG_PATH, self.image_path, f"{uuid4()}.qcow2")
 		self.file_path = str(image_path.absolute())
+		frappe.db.set_value(self.doctype, self.name, "file_path", self.file_path)
 		frappe.db.commit()
 
 		self.just_copy = False
